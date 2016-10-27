74c3c2d3531a1337f4aa2dfb724a48b0ebabac3a
ConnorFieldUser <conquest.field@gmail.com>
2016-10-27 15:05:23 -0400

    finally got my app working. It only took 3 hours.

--- The_Iron_Bank/bank_records_app/views.py
+++ The_Iron_Bank/bank_records_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = "/"
